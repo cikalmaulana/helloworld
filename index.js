99dc63681e29cf912dfe2b0a980615a9bf5e4941
Rajasa Cikal <79238217+cikalmaulana@users.noreply.github.com>
2024-01-31 09:07:16 +0700

    Add meja3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Cikal Ganteng2" }');
+    res.send('{ "response": "Cikal Ganteng Banget" }');
 });
 
 app.get('/will', function (req, res) {

9b68df41ae9278e01afb460031b4abce0a7eff16
Rajasa Cikal <79238217+cikalmaulana@users.noreply.github.com>
2024-02-01 15:56:05 +0700

    PAWTB-19 Cikal Ganteng

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Cikal Ganteng Banget" }');
+    res.send('{ "response": "Hi" }');
 });
 
 app.get('/will', function (req, res) {
